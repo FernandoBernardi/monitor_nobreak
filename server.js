336bf3d5886709741e6573443ac8ce9170fae747
fernandobl-046 <fernandoluis.lucas@gmail.com>
2021-01-12 08:02:13 -0300

    merge com o antigo

--- server.js
+++ server.js
@@ -1,65 +1,61 @@
 const http = require('http');
 const mariadb = require('mariadb');
-const options = {
-    host: '127.0.0.1',
-    path: '/text.cgi'
+const optionsNobreak1 = {
+    host: '192.168.0.1',
+    port: '64111',
+    path: '/medicoes.cgi'
 }
-let optionsDB = {
-  host: '127.0.0.1', 
+const optionsNobreak2 = {
+  host: '192.168.0.1',
+  port: '64112',
+  path: '/medicoes.cgi'
+}
+const optionsDB = {
+  host: '127.0.0.1',
   port: '3306',
-  user:'root', 
+  user:'root',
   password: '',
-  database: 'dataNobreak',
+  database: 'logsnobreak',
   connectionLimit: 5
 }
+
 setInterval(() => {
-    let request = http.request(options, (res) => {
-        let data = '';
-        res.on('data', (atualStage) => {
-            data += atualStage;
-        });        
-        res.on('end', async () => {          
-          try{
-            await asyncFunction(data, "logsnobreak");
-          }catch(err){
-            console.error(err);
-            throw(err);
-          }
-        });
-    });
-    request.on('error', (e) => {
-        console.log(e.message);
-    });
-    request.end();
-    let request2 = http.request(options, function (res) {
-      let data2 = '';
-      res.on('data', (atualStage) => {
-          data2 += atualStage;
-      });        
-      res.on('end', async () => {          
-        try{
-          await asyncFunction(data2, "logsnobreak2");
-        }catch(err){
-          console.error(err);
-          throw(err);
-        }
-      });
-    });
-    request2.on('error', (e) => {
+  http.get(optionsNobreak1, (res) => {
+    let data = '';
+    res.on('data', (collect) => { data += collect; });
+    res.on('end', () => {
+      try {
+        asyncFunction(data, "logsnobreak");
+      } catch (e) {
         console.error(e.message);
+      }
     });
-    request2.end();
+}).on("error", (e) => {
+  console.error("Error: " + e.message);
+});
+
+  http.get(optionsNobreak2, (res) => {
+    let data = '';
+    res.on('data', (collect) => { data += collect; });
+    res.on('end', () => {
+      try {
+        asyncFunction(data, "logsnobreak2");
+      } catch (e) {
+        console.error(e.message);
+      }
+    });
+  }).on("error", (e) => {
+  console.error("Error: " + e.message);
+  });
 }, 1000);
 
 async function asyncFunction(dataref,tabela){
-    let tratamento = dataref.replace(/[\(,\),\%]/g,"");
-    let arrayData = tratamento.split(/\s/g);
-    const dataAtual = new Date();
-    const sqlData = "INSERT INTO "+tabela+" (dataTempo,valor1,valor2,valor3,valor4,valor5,valor6,valor7,valor8,valor9,valor10,valor11,valor12,valor13,valor14,valor15,valor16,valor17) VALUES ?";
-    let valuesData = [dataAtual,arrayData[0],arrayData[1],arrayData[2],arrayData[3],arrayData[4],arrayData[5],arrayData[6],arrayData[7],arrayData[8],arrayData[9],arrayData[10],arrayData[11],arrayData[12],arrayData[13],arrayData[14],arrayData[15],arrayData[16]];
-    mariadb.createConnection(optionsDB)
+  const tratamento = dataref.replace(/[\(,\),\%]/g,"").replace("&#176;","");
+  const arrayData = tratamento.split(/\s/g);
+  const sqlTab = `INSERT INTO ${tabela} (entradaR,entradaS,entradaT,entradaFreq,bypassR,bypassS,bypassT,freqBypass,saidaR,saidaS,saidaT,freqSaida,potSaidaAparentR,potSaidaAparentS,potSaidaAparentT,potSaidaAtivaR,potSaidaAtivaS,potSaidaAtivaT,correnteSaidaR,correnteSaidaS,correnteSaidaT,barramento,bateria,temperatura) VALUES (${arrayData[0]},${arrayData[2]},${arrayData[4]},${arrayData[6]},${arrayData[8]},${arrayData[10]},${arrayData[12]},${arrayData[14]},${arrayData[16]},${arrayData[18]},${arrayData[20]},${arrayData[22]},${arrayData[24]},${arrayData[27]},${arrayData[30]},${arrayData[33]},${arrayData[36]},${arrayData[39]},${arrayData[42]},${arrayData[45]},${arrayData[48]},${arrayData[51]},${arrayData[53]},${arrayData[55]})`;
+  mariadb.createConnection(optionsDB)
     .then(conn => {
-      conn.query(sqlData, [valuesData])
+      conn.query(sqlTab)
         .then(() => {
           conn.end();          
         })
@@ -73,3 +69,13 @@ async function asyncFunction(dataref,tabela){
       throw err;
     });
   }
+
+  // process.on('SIGINT', async () => {
+  //   let status = 0
+  //   try {
+  //     await createConnection.end();
+  //   } catch (err) {
+  //     status = 1;
+  //   }
+  //   process.exit(status)
+  // });
