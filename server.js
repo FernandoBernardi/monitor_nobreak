56667cf8e9284e7c2b69fd814844d4ff437e8598
fernandobl-046 <fernandoluis.lucas@gmail.com>
2021-01-21 13:41:35 -0300

    resolve async

--- server.js
+++ server.js
@@ -56,14 +56,14 @@ async function asyncFunction(dataref,tabela){
   const sqlTab = `INSERT INTO ${tabela} (entradaR,entradaS,entradaT,entradaFreq,bypassR,bypassS,bypassT,freqBypass,saidaR,saidaS,saidaT,freqSaida,potSaidaAparentR,potSaidaAparentS,potSaidaAparentT,potSaidaAtivaR,potSaidaAtivaS,potSaidaAtivaT,correnteSaidaR,correnteSaidaS,correnteSaidaT,barramento,bateria,temperatura) VALUES (${arrayData[0]},${arrayData[2]},${arrayData[4]},${arrayData[6]},${arrayData[8]},${arrayData[10]},${arrayData[12]},${arrayData[14]},${arrayData[16]},${arrayData[18]},${arrayData[20]},${arrayData[22]},${arrayData[24]},${arrayData[27]},${arrayData[30]},${arrayData[33]},${arrayData[36]},${arrayData[39]},${arrayData[42]},${arrayData[45]},${arrayData[48]},${arrayData[51]},${arrayData[53]},${arrayData[55]})`;
   let conn;
   try{
-    conn = conexaorMariadb.getConnection();
-    (await conn).query(sqlTab)    
-  }catch(e){
-    console.log(e);
-    throw e;
-  }
-  if(conn){
-    (await conn).end();
+    conn = await conexaorMariadb.getConnection();
+    await conn.query(sqlTab);   
+  }catch(err){
+    throw err;
+  } finally {
+    if (conn){
+     return conn.release();
+    }      
   }
 }
 
