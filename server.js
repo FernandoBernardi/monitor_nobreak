72b0bbec48f7238319c14c4e5f9eac9657ccff36
fernandobl-046 <fernandoluis.lucas@gmail.com>
2021-01-27 17:24:43 -0300

    corrigido layout

--- server.js
+++ server.js
@@ -2,57 +2,54 @@ const mariadb = require('mariadb');
 const axios = require('axios');
 //Conexão com o banco e url para pegar as informações
 const optionsDB = {
-  host: '127.0.0.1',
-  port: '3306',
-  user:'root',
-  password: '',
-  database: 'logsnobreak',
-  connectionLimit: 5
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: '',
+    database: 'logsnobreak',
+    connectionLimit: 5
 }
 const conexaorMariadb = mariadb.createPool(optionsDB);
 
 // Função para buscar informações servidor
-async function get_nobreak(){
-  try{
-    // Nobreak 1
-    await axios.get("http://192.168.0.1:64111/medicoes.cgi")
-    .then(response => {
-      asyncFunction(response.data, "logsnobreak");
-    })
-    .catch(error => {
-      console.log(error);
-    })
-    // Nobreak 2
-    await axios.get("http://192.168.0.1:64112/medicoes.cgi")
-    .then(response => {
-      asyncFunction(response.data, "logsnobreak2");
-    })
-    .catch(error => {
-      console.log(error);
-    })
-  }catch(error){
-    console.log(error);
-  }
+async function get_nobreak() {
+    try {
+        // Nobreak 1
+        await axios.get("http://192.168.0.1:64111/medicoes.cgi")
+            .then(response => {
+                asyncFunction(response.data, "logsnobreak");
+            })
+            .catch(error => {
+                console.error(error);
+            })
+            // Nobreak 2
+        await axios.get("http://192.168.0.1:64112/medicoes.cgi")
+            .then(response => {
+                asyncFunction(response.data, "logsnobreak2");
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    } catch (error) {
+        console.error(error);
+    }
 }
 // Armazenar no banco de dados as informações
-async function asyncFunction(dataref,tabela){
-  const tratamento = dataref.replace(/[\(,\),\%]/g,"").replace("&#176;","");
-  const arrayData = tratamento.split(/\s/g);
-  const sqlTab = `INSERT INTO ${tabela} (entradaR,entradaS,entradaT,entradaFreq,bypassR,bypassS,bypassT,freqBypass,saidaR,saidaS,saidaT,freqSaida,potSaidaAparentR,potSaidaAparentS,potSaidaAparentT,potSaidaAtivaR,potSaidaAtivaS,potSaidaAtivaT,correnteSaidaR,correnteSaidaS,correnteSaidaT,barramento,bateria,temperatura) VALUES (${arrayData[0]},${arrayData[2]},${arrayData[4]},${arrayData[6]},${arrayData[8]},${arrayData[10]},${arrayData[12]},${arrayData[14]},${arrayData[16]},${arrayData[18]},${arrayData[20]},${arrayData[22]},${arrayData[24]},${arrayData[27]},${arrayData[30]},${arrayData[33]},${arrayData[36]},${arrayData[39]},${arrayData[42]},${arrayData[45]},${arrayData[48]},${arrayData[51]},${arrayData[53]},${arrayData[55]})`;
-  let conn;
-  try{
-    conn = await conexaorMariadb.getConnection();
-    await conn.query(sqlTab);   
-  } catch(err){
-    console.log(err);
-    throw err;
-  } finally {
-    if (conn){
-     conn.end();
-    }      
-  }
+async function asyncFunction(dataref, tabela) {
+    let conn;
+    try {
+        const tratamento = dataref.replace(/[\(,\),\%]/g, "").replace("&#176;", "");
+        const arrayData = tratamento.split(/\s/g);
+        const sqlTab = `INSERT INTO ${tabela} (entradaR,entradaS,entradaT,entradaFreq,bypassR,bypassS,bypassT,freqBypass,saidaR,saidaS,saidaT,freqSaida,potSaidaAparentR,potSaidaAparentS,potSaidaAparentT,potSaidaAtivaR,potSaidaAtivaS,potSaidaAtivaT,correnteSaidaR,correnteSaidaS,correnteSaidaT,barramento,bateria,temperatura) VALUES (${arrayData[0]},${arrayData[2]},${arrayData[4]},${arrayData[6]},${arrayData[8]},${arrayData[10]},${arrayData[12]},${arrayData[14]},${arrayData[16]},${arrayData[18]},${arrayData[20]},${arrayData[22]},${arrayData[24]},${arrayData[27]},${arrayData[30]},${arrayData[33]},${arrayData[36]},${arrayData[39]},${arrayData[42]},${arrayData[45]},${arrayData[48]},${arrayData[51]},${arrayData[53]},${arrayData[55]})`;
+        conn = await conexaorMariadb.getConnection();
+        await conn.query(sqlTab);
+    } catch (err) {
+        console.error(err);
+    } finally {
+        if (conn) return conn.end();
+    }
 }
 // Intervalo com Promisse para aguardar o resultado e não criar várias Instâncias!
-setInterval(async () => {
-  await get_nobreak();
+setInterval(async() => {
+    await get_nobreak();
 }, 1000)
